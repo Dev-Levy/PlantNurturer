a326c832104643bd898422baaf66fb6f93a800c8
Oláh Levente <olahlevente1016@gmail.com>
2026-04-20 13:37:53 +0200

    added luc value, commented out soiltemp

--- lib/sensors/structs/sensorReadings.h
+++ lib/sensors/structs/sensorReadings.h
@@ -2,9 +2,10 @@
 
 struct SensorReadings
 {
-    int16_t airHumidity;
     int16_t airTemp;
-    int16_t soilTemp;
+    int16_t airHumidity;
+    // int16_t soilTemp;
     uint8_t soilMoisture;
     uint8_t light : 1;
+    uint16_t lightLux;
 } __attribute__((packed));
