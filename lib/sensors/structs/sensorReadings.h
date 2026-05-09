8cf2912d0445a03fd9829ac65530319819d8b872
Oláh Levente <olahlevente1016@gmail.com>
2026-05-09 20:11:42 +0200

    made moist uint16

--- lib/sensors/structs/sensorReadings.h
+++ lib/sensors/structs/sensorReadings.h
@@ -5,7 +5,7 @@ struct SensorReading
     int16_t airTemp;
     int16_t airHumidity;
     int16_t soilTemp;
-    uint8_t soilMoisture;
+    uint16_t soilMoisture;
     uint8_t light : 1;
     uint16_t lightLux;
 } __attribute__((packed));
